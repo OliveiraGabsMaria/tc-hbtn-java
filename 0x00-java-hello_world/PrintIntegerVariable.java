2e021d136a1613800048b54b1049127afd0d318f
Gabrielly Oliveira <gabriellyorosimbooliveira@gmail.com>
2022-05-02 06:36:27 -0700

    Imprimir task 2

---
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -0,0 +1,11 @@
+
+public class PrintIntegerVariable {
+
+	public static void main (String[] args) {
+
+		int numero =  311;
+		String endereco = "Avenida Andaló, ";
+
+		System.out.println(endereco + numero);
+	}
+}
