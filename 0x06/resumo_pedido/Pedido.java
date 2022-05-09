be0990c68caeb32dcdbe8c50cb750d4db2371f57
Gabrielly Oliveira <gabriellyorosimbooliveira@gmail.com>
2022-05-09 13:39:05 -0300

    Imprimir task 1

--- 0x06/resumo_pedido/Pedido.java
+++ 0x06/resumo_pedido/Pedido.java
@@ -1,47 +1,47 @@
-import produtos.Produto;
-import produtos.Livro;
 import produtos.Dvd;
+import produtos.Livro;
+import produtos.Produto;
 
 public class Pedido {
+
     private double percentualDesconto;
     private ItemPedido[] itens;
 
-    public Pedido(int percentualDesconto, ItemPedido[] itens){
+    public Pedido(double percentualDesconto, ItemPedido[] itens) {
         this.percentualDesconto = percentualDesconto;
         this.itens = itens;
     }
 
-    public double calcularTotal(){
+    public double calcularTotal() {
         double total = 0;
 
-        for(ItemPedido item : itens){
-            total += item.getQuantidade()*(item.getProduto().obterPrecoLiquido());
+        for (ItemPedido item : itens) {
+            total += item.getQuantidade() * (item.getProduto().obterPrecoLiquido());
         }
-
         return total - total * this.percentualDesconto / 100;
     }
 
-    public void apresentarResumoPedido(){
+    public void apresentarResumoPedido() {
         double total = 0;
         double desconto = 0;
 
         System.out.println("------- RESUMO PEDIDO -------");
 
-        for(ItemPedido item: itens){
-            Produto produto = item.getProduto;
+        for (ItemPedido item : itens) {
+            Produto p = item.getProduto();
 
-            if (produto instanceof Livro) {
+            if (p instanceof Livro) {
                 System.out.printf("Tipo: Livro ");
-            }
-            else if (produto instanceof Dvd) {
+            } else if (p instanceof Dvd) {
                 System.out.printf("Tipo: Dvd ");
             }
-            System.out.printf("Titulo: " + produto.getTItulo() + " ");
-            System.out.printf("Preco: " + String.format("%.2f" , produto.obterPrecoLiquido()) + " ");
-            System.out.printf("Quant: " + item.getQuantidade() + " ");
-            System.out.printf("Total: " + String.format("%.2f", produto.obterPrecoLiquido() * item.getQuantidade()) + "/n");
 
-            total += produto.obterPrecoLiquido() * item.getQuantidade();
+            System.out.printf("Titulo: " + p.getTitulo()+ " ");
+            System.out.printf("Preco: " + String.format("%.2f" , p.obterPrecoLiquido()) + " ");
+            System.out.printf("Quant: " + item.getQuantidade() + " ");
+            System.out.printf("Total: " + String.format("%.2f", p.obterPrecoLiquido() * item.getQuantidade()) + "/n");
+
+            total += p.obterPrecoLiquido() * item.getQuantidade();
         }
 
         desconto = this.percentualDesconto * total/100;
@@ -53,5 +53,7 @@ public class Pedido {
         System.out.println("\n----------------------------");
         System.out.printf("TOTAL PEDIDO: " + String.format("%.2f", total - desconto));
         System.out.println("\n----------------------------");
+
+        }
     }
-}
+
