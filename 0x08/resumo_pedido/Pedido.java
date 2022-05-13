2474a1ba9613337062cfa1da869cf3ea298ffb69
Gabrielly Oliveira <gabriellyorosimbooliveira@gmail.com>
2022-05-13 10:59:02 -0300

    Imprimir task 0

--- 0x08/resumo_pedido/Pedido.java
+++ 0x08/resumo_pedido/Pedido.java
@@ -2,19 +2,47 @@ import provedores.Frete;
 
 public class Pedido {
 
-     double codigo;
+     int codigo;
      double peso;
      double total;
      Frete frete;
 
-    public Pedido(double codigo, double peso, double total) {
+    public Pedido(int codigo, double peso, double total) {
         this.codigo = codigo;
         this.peso = peso;
         this.total = total;
     }
 
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
     public Frete getFrete() {
-        return this.frete;
+        return frete;
+    }
+
+    public void setFrete(Frete frete) {
+        this.frete = frete;
     }
 }
  
