f68867c903a49ece9dd9e86efc8e93d5262f0f6d
Gabrielly Oliveira <gabriellyorosimbooliveira@gmail.com>
2022-05-24 16:50:41 -0300

    Imprimir task 0

--- Generics_exercises/armazem/Eletronico.java
+++ Generics_exercises/armazem/Eletronico.java
@@ -3,7 +3,7 @@ public class Eletronico {
     private String descricao;
     private double valor;
 
-    private Eletronico(String descricao, double valor) {
+    public Eletronico(String descricao, double valor) {
         this.descricao = descricao;
         this.valor = valor;
     }
